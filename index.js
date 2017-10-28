799d0024d87a70dfec76f804c7e96c5d57cee2bd
Elise Bonner <elise0209@mac.com>
2017-10-28 17:18:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function add(){
+  
+}
+
+function subtract() {
+  
+}
+
+function multiply(){
+
+}
+
+function divide(){
+  
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
+
+function makeInt (n) {
+  
+}
+
+function preserveDecimal(n){
+  
+}
