9d57afc7bc6dc6dcfe6bbe60c0788ad1c72dfc82
Elise Bonner <elise0209@mac.com>
2017-10-28 17:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ var a = Math.floor(Math.random() * 1000)
 var b = Math.floor(Math.random() * 1000)
 
 function add(){
-  
+  return a += b;
 }
 
 function subtract() {
-  
+ return a -= b;
 }
 
 function multiply(){
-
+ return a *= b;
 }
 
 function divide(){
-  
+ return a /= b;
 }
 
 function inc(n) {
-  
+ return a++;
 }
 
 function dec(n) {
-  
+ return a --;
 }
 
 function makeInt (n) {
-  
+  return parseInt (n, 10);
 }
 
 function preserveDecimal(n){
-  
+ return parseFloat (n, 10)
 }
